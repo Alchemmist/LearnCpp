083ce42767cc5a1fa30af101af42578f2240bc0c
Anthropophag <anton.ingrish@gmail.com>
2023-01-01 19:24:52 +0300

    commit

---
+++ Первые шаги/studetnbook.cpp
@@ -0,0 +1,24 @@
+/* #include <iostream>
+
+int main() {
+  std::cout << "Hello world!\n";
+  return 0;
+} */
+
+/*#include <string>
+
+int main() {
+  int value;
+  value = 42;
+  std::string title = 'si';
+}*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string name;
+  std::cout << "Whts your name?\n";
+  std::cin >> name;
+  std::cout << "Hello, " << name << "!\n";
+}
