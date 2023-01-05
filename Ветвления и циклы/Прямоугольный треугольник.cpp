69a12d6b06726e1f6390c370cc61d55262e8657a
Anthropophag <anton.ingrish@gmail.com>
2023-01-05 15:35:33 +0300

    commit

--- Ветвления и циклы/Прямоугольный треугольник.cpp
+++ Ветвления и циклы/Прямоугольный треугольник.cpp
@@ -9,5 +9,12 @@ int main() {
   cin >> b;
   cin >> c;
   //
-  if 
+  if (a * a + b * b == c * c || c * c + a * a == b * b ||
+      c * c + b * b == a * a) {
+    cout << "YES";
+  } else if (a + b <= c || c + a <= b || c + b <= a) {
+    cout << "UNDEFINED";
+  } else {
+    cout << "NO";
+  }
 }
