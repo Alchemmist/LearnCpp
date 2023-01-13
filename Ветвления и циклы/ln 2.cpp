bc87a4b91c058623c41240f2eb24635327541975
Alchemmist <anton.ingrish@gmail.com>
2023-01-13 06:52:46 +0300

    commit

--- Ветвления и циклы/ln 2.cpp
+++ Ветвления и циклы/ln 2.cpp
@@ -1,4 +1,23 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
-int main() {}
+int main() {
+  int n;
+  int m;
+  cin >> n;
+  m = 1;
+  bool operation = true;
+  double finish = 0.0;
+  //
+  while (m != n) {
+    if (operation) {
+      finish += (double)1 / m;
+    } else {
+      finish -= (double)1 / m;
+    }
+    m += 1;
+    operation = not operation;
+  }
+  cout << finish;
+}
